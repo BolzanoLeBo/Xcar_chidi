063b83e6cc4108edf22f03234f3f2c79f78c19aa
BolzanoLeBo <loic.thomas1509@gmail.com>
2023-11-27 17:23:09 +0100

    modification of the img_processing node

--- jetsonNano/ros2_ws/src/img_processing/img_processing/img_processing_node.py
+++ jetsonNano/ros2_ws/src/img_processing/img_processing/img_processing_node.py
@@ -1,27 +1,49 @@
 import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
+
 from cv_bridge import CvBridge
 import cv2
 
-def image_callback(msg):
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+class ImgProcessing(Node):
+    def __init__(self):
+        super().__init__('img_processing')
+        self.subscriber_ = self.create_subscription(Image, 'image_raw', self.image_callback)
+    
 
-    # Your image processing or display logic here
-    #cv2.imshow("Received Image", cv_image)
-    cv2.imwrite("img/img.png", cv_image)
+    def image_callback(msg):
+        self.get_logger().info("img callback")
+        bridge = CvBridge()
+        
+        cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
-def main():
-    rclpy.init()
+        # Your image processing or display logic here
+        #cv2.imshow("Received Image", cv_image)
+        cv2.imwrite("img/img.png", cv_image)
 
-    node = rclpy.create_node('image_subscriber_node')
-    subscription = node.create_subscription(
-        Image, 'image_raw', image_callback, 10)
-    # Adjust the queue size and topic according to your needs
+    def run(self):
+        # Main loop of the node
+        self.get_logger().info("Run img processing")
+        while rclpy.ok():
+            # Your node's main functionality goes here
+            rclpy.spin_once(self, timeout_sec=0.1)
 
-    rclpy.spin(node)
+        # Clean up when the node is shut down
+        self.destroy_node()
+        rclpy.shutdown()
 
+def main(args=None):
+    rclpy.init(args=args)
+
+    img_processing = ImgProcessing()
+    img_processing.run()
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    joystick_ros2.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
