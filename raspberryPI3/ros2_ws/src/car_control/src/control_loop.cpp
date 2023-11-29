a643049d54aae8cbf7271afdf8c9ff71eb9c598a
NicoSiard <nico.siard@gmail.com>
2023-11-29 14:08:13 +0100

    top

--- raspberryPI3/ros2_ws/src/car_control/src/control_loop.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/control_loop.cpp
@@ -18,7 +18,10 @@ void recurrence_equation(double Error, double& Error_last, double& PWM_order, do
     ///Error=Error*0.9;
 
     // Corrected PWM 
+
     PWM_order = PWM_order_last + 9.025*Error + 8.975*Error_last;
+    RCLCPP_INFO(this->get_logger(), std::to_string(PWM_order).c_str());
+
 
     // Error Update
     Error_last=Error;
