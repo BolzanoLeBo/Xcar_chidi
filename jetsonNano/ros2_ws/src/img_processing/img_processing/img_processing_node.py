579bfe0ef29f1b95faf87a5124fe3a133fa7ac49
BolzanoLeBo <loic.thmas1509@gmail.com>
2023-12-01 11:25:04 +0100

    ça marche

--- jetsonNano/ros2_ws/src/img_processing/img_processing/img_processing_node.py
+++ jetsonNano/ros2_ws/src/img_processing/img_processing/img_processing_node.py
@@ -3,14 +3,12 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
-from interfaces import TrackingPosAngle
-
-from img_tools import *
+from interfaces.msg import TrackingPosAngle
 
 from cv_bridge import CvBridge
 import cv2
 
-class ImgProcessing(Node):
+class ImgProcessingNode(Node):
 	def __init__(self):
 		super().__init__('img_processing_node')
 		qos_profile = QoSProfile(
@@ -19,8 +17,8 @@ class ImgProcessing(Node):
 			depth=1
 		)
 		self.subscriber_ = self.create_subscription(Image, 'image_raw', self.image_callback,qos_profile = qos_profile)
-		self.tracking_pos_angle_publisher_ = self.create_publisher(TrackingPosAngle,'/tracking_pos_angle',qos_profile = qos_profile)
-
+		self.tracking_pos_angle_publisher_ = self.create_publisher(TrackingPosAngle,'tracking_pos_angle', 10)
+		
 
 
 
@@ -28,41 +26,24 @@ class ImgProcessing(Node):
 
 
 	def image_callback(self,msg):
-		self.get_logger().info("img callback")
 		bridge = CvBridge()
-		msg = TrackingPosAngle()
-		msg.min_angle = 1
-		msg.max_angle = 2
+		tracking = TrackingPosAngle()
+		tracking.min_angle = 1.0
+		tracking.max_angle = 2.0
 		
 		cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
 		# Your image processing or display logic here
 		#cv2.imshow("Received Image", cv_image)
-		cv2.imwrite("/img.png", cv_image)
+		#cv2.imwrite("/img.png", cv_image)
 
 		# Publish the msg for angle
-		self.tracking_pos_angle_publisher_.publish(msg)
-
-	def run(self):
-		# Main loop of the node
-		self.get_logger().info("Run img processing")
-		while rclpy.ok():
-			# Your node's main functionality goes here
-			rclpy.spin_once(self, timeout_sec=0.1)
-
-		# Clean up when the node is shut down
-		self.destroy_node()
-		rclpy.shutdown()
+		self.tracking_pos_angle_publisher_.publish(tracking)
 
 def main(args=None):
 	rclpy.init(args=args)
-
-	img_processing_node = ImgProcessing()
-	img_processing_node.run()
-
-	# Destroy the node explicitly
-	# (optional - otherwise it will be done automatically
-	# when the garbage collector destroys the node object)
+	img_processing_node = ImgProcessingNode()
+	rclpy.spin(img_processing_node)
 	img_processing_node.destroy_node()
 	rclpy.shutdown()
 
--- jetsonNano/ros2_ws/src/img_processing/setup.py
+++ jetsonNano/ros2_ws/src/img_processing/setup.py
@@ -5,7 +5,7 @@ package_name = 'img_processing'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude="tests" ),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
