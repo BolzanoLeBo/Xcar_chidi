863617f574f16e662f09e0733deb67c34efa340f
BolzanoLeBo <loic.thomas1509@gmail.com>
2023-12-01 08:27:43 +0100

    add ai tools in the img processing repository

--- jetsonNano/ros2_ws/src/img_processing/img_processing/img_processing_node.py
+++ jetsonNano/ros2_ws/src/img_processing/img_processing/img_processing_node.py
@@ -5,65 +5,67 @@ from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
 from interfaces import TrackingPosAngle
 
+from img_tools import *
+
 from cv_bridge import CvBridge
 import cv2
 
 class ImgProcessing(Node):
-    def __init__(self):
-        super().__init__('img_processing_node')
-        qos_profile = QoSProfile(
-            reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
-            history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
-            depth=1
-        )
-        self.subscriber_ = self.create_subscription(Image, 'image_raw', self.image_callback,qos_profile = qos_profile)
-        self.tracking_pos_angle_publisher_ = self.create_publisher(TrackingPosAngle,'/tracking_pos_angle',qos_profile = qos_profile)
+	def __init__(self):
+		super().__init__('img_processing_node')
+		qos_profile = QoSProfile(
+			reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+			history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+			depth=1
+		)
+		self.subscriber_ = self.create_subscription(Image, 'image_raw', self.image_callback,qos_profile = qos_profile)
+		self.tracking_pos_angle_publisher_ = self.create_publisher(TrackingPosAngle,'/tracking_pos_angle',qos_profile = qos_profile)
 
 
 
 
-    #here we define how to update the angle value of the tracking
+	#here we define how to update the angle value of the tracking
 
 
-    def image_callback(self,msg):
-        self.get_logger().info("img callback")
-        bridge = CvBridge()
-        msg = TrackingPosAngle()
-        msg.min_angle = 1
-        msg.max_angle = 2
-        
-        cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+	def image_callback(self,msg):
+		self.get_logger().info("img callback")
+		bridge = CvBridge()
+		msg = TrackingPosAngle()
+		msg.min_angle = 1
+		msg.max_angle = 2
+		
+		cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
-        # Your image processing or display logic here
-        #cv2.imshow("Received Image", cv_image)
-        cv2.imwrite("/img.png", cv_image)
+		# Your image processing or display logic here
+		#cv2.imshow("Received Image", cv_image)
+		cv2.imwrite("/img.png", cv_image)
 
-        # Publish the msg for angle
-        self.tracking_pos_angle_publisher_.publish(msg)
+		# Publish the msg for angle
+		self.tracking_pos_angle_publisher_.publish(msg)
 
-    def run(self):
-        # Main loop of the node
-        self.get_logger().info("Run img processing")
-        while rclpy.ok():
-            # Your node's main functionality goes here
-            rclpy.spin_once(self, timeout_sec=0.1)
+	def run(self):
+		# Main loop of the node
+		self.get_logger().info("Run img processing")
+		while rclpy.ok():
+			# Your node's main functionality goes here
+			rclpy.spin_once(self, timeout_sec=0.1)
 
-        # Clean up when the node is shut down
-        self.destroy_node()
-        rclpy.shutdown()
+		# Clean up when the node is shut down
+		self.destroy_node()
+		rclpy.shutdown()
 
 def main(args=None):
-    rclpy.init(args=args)
+	rclpy.init(args=args)
 
-    img_processing_node = ImgProcessing()
-    img_processing_node.run()
+	img_processing_node = ImgProcessing()
+	img_processing_node.run()
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    img_processing_node.destroy_node()
-    rclpy.shutdown()
+	# Destroy the node explicitly
+	# (optional - otherwise it will be done automatically
+	# when the garbage collector destroys the node object)
+	img_processing_node.destroy_node()
+	rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    main()
+	main()
